e41a9639d0ff584eb2804abfd74b65a4f90c4c44
Juan Andres Alvarez <juanandres1029@gmail.com>
2024-12-05 10:04:11 -0500

    Añade DataTable paravisualización de datos

--- data/datos.js
+++ data/datos.js
@@ -1,24 +1,35 @@
 let datosPaises = [];
 const tablaPaises = document.getElementById('datosPaises');
+const dataBtn = document.getElementById('dataBtn');
 
-fetch("./data/datos.json")
+dataBtn.addEventListener('click', cargarData);
+
+function cargarData(){
+    fetch("./data/datos.json")
 .then((res)=> res.json())
 .then((data)=>{
     datosPaises = data;
-    let colombia = datosPaises.filter((pais) => pais.Entity === "Colombia");
-    cargarTabla(colombia)//datosPaises.slice(0,50)
+    //let colombia = datosPaises.filter((pais) => pais.Entity === "Colombia");
+    cargarTabla(datosPaises)//datosPaises.slice(0,50)
+    new DataTable('#tablaPaises');
 })
 .catch((err)=>{
     console.log("Error al cargar los datos",err);
 });
+}
 
-function cargarTabla(datos){
-    datos.forEach(({Entity, Code, Year,'Solar (% equivalent primary energy)': Solar },index)=>{
-        tablaPaises.innerHTML +=`<tr>
-        <td>${Entity}</td>
-        <td>${Code}</td>
-        <td>${Year}</td>
-        <td>${Solar ? Solar.toFixed(6) : 'N/A'}</td> <!-- Mostrar N/A si no hay valor -->
-      </tr>`
-    })
+
+function cargarTabla(datos) {
+    let filasTabla = '';
+    datos.forEach(({ Entity, Code, Year, 'Electricity from solar (TWh)': Solar }) => {
+        filasTabla += `
+            <tr>
+                <td>${Entity}</td>
+                <td>${Code}</td>
+                <td>${Year}</td>
+                <td>${Solar ? Solar.toFixed(6) : 'N/A'}</td>
+            </tr>
+        `;
+    });
+    tablaPaises.innerHTML = filasTabla;
 }
